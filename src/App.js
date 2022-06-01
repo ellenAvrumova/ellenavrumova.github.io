ee50aeffbebacbe959907f3d35099a20baca5085
ellenAvrumova <e.avrumova@gmail.comm>
2022-06-01 18:42:21 -0400

    Beginning setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2><center>Ellen Avrumova</center></h2>
+        <p1>Hello! I'm a freshman at Hunter College.<br></br>I'm part of the Daedalus Program.</p1>
+        <a href='https://stackoverflow.com/questions/4908171/how-to-show-local-picture-in-web-page'>here</a>
       </header>
     </div>
   );
