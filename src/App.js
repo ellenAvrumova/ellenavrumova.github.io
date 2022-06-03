3e1be19f5cd45cc1c4f0ec9c8c393f8f13ec6d9c
ellenAvrumova <e.avrumova@gmail.comm>
2022-06-03 16:15:50 -0400

    Adding socials

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { SocialIcon } from 'react-social-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2><center>Ellen Avrumova</center></h2>
+      <SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15,height: 50, width: 50 }}/>
+      <SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15 }}/> 
+      <h2><center>Ellen Avrumova</center></h2>
         <p1>Hello! I'm a freshman at Hunter College.<br></br>I'm part of the Daedalus Program.</p1>
-        <a href='https://stackoverflow.com/questions/4908171/how-to-show-local-picture-in-web-page'>here</a>
       </header>
     </div>
   );
