268d0bcb37dcb33d1bbc47cf3d3c416d5294e3b6
ellenAvrumova <e.avrumova@gmail.comm>
2022-06-03 19:11:32 -0400

    Adding socials

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15,height: 50, width: 50 }}/>
-      <SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15 }}/> 
-      <h2><center>Ellen Avrumova</center></h2>
+        <SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15 }}/>
+        <SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15 }}/>
+      <h2><center> Ellen Avrumova</center></h2>
         <p1>Hello! I'm a freshman at Hunter College.<br></br>I'm part of the Daedalus Program.</p1>
       </header>
     </div>
