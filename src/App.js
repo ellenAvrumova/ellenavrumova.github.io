fc31f043402d04d8d313a21a8781474739dee658
ellenAvrumova <e.avrumova@gmail.comm>
2022-06-04 18:35:54 -0400

    Added hi image

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import myImage from './myImage.jpg'
+import hi from './Hi.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Container>
-          <Row>
-            <Col><SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15, left:100 }}/></Col>
-            <Col><SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15, left:100 }}/></Col>
-            <Col><SocialIcon url="mailto:e.avrumova@gmail.com?" label="Gmail" style={{ margin:15, left:100 }} bgColor="#de5246"/></Col>
-          </Row>
-        </Container>
-        <Container>
-          <Row>
-            <Col>
-              <h2>Ellen Avrumova</h2>
-            </Col>
-            <Col sm = {{ offset: 5, span: 5 }}>
-              <img width="100%" src={myImage} alt="Me" />
-            </Col>
-          </Row>
-        </Container>
-        <Container>
-
-        </Container>
+    <Container>
+      <Row>
+        <Col><SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15, left:100 }}/></Col>
+        <Col><SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15, left:100 }}/></Col>
+        <Col><SocialIcon url="mailto:e.avrumova@gmail.com?" label="Gmail" style={{ margin:15, left:100 }} bgColor="#de5246"/></Col>
+      </Row>
+    </Container>
+    <Container>
+      <Row>
+        <Col sm= {{ offset: 2, span:2 }}>
+          <img width="100%" src={hi} alt="hello" />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm = {{ span: 4 }}>
+          <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem' }} />
+        </Col>
+      </Row>
+    </Container>
       </header>
     </div>
   );
