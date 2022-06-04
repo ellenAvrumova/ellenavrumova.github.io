e4bde1cf2085704051e191c6800495bbf5c39418
ellenAvrumova <e.avrumova@gmail.comm>
2022-06-03 23:15:42 -0400

    Added socials

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import { SocialIcon } from 'react-social-icons'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15 }}/>
-        <SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15 }}/>
+      <Container>
+      <Row>
+        <Col><SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15, left:100 }}/></Col>
+        <Col><SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15, left:100 }}/></Col>
+        <Col><SocialIcon url="mailto:e.avrumova@gmail.com?" label="Gmail" style={{ margin:15, left:100 }} bgColor="#de5246"/></Col>
+      </Row>
+    </Container>
       <h2><center> Ellen Avrumova</center></h2>
         <p1>Hello! I'm a freshman at Hunter College.<br></br>I'm part of the Daedalus Program.</p1>
       </header>
