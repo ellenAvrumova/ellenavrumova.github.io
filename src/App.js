4b0d24609a42687dad31b6213b18bad3feca81b8
ellenAvrumova <e.avrumova@gmail.comm>
2022-06-04 20:40:10 -0400

    Added typing

--- src/App.js
+++ src/App.js
@@ -11,25 +11,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-    <Container>
-      <Row>
-        <Col><SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15, left:100 }}/></Col>
-        <Col><SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15, left:100 }}/></Col>
-        <Col><SocialIcon url="mailto:e.avrumova@gmail.com?" label="Gmail" style={{ margin:15, left:100 }} bgColor="#de5246"/></Col>
-      </Row>
-    </Container>
-    <Container>
-      <Row>
-        <Col sm= {{ offset: 2, span:2 }}>
-          <img width="100%" src={hi} alt="hello" />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm = {{ span: 4 }}>
-          <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem' }} />
-        </Col>
-      </Row>
-    </Container>
+
+        <Container>
+          <Row>
+            <Col><SocialIcon url="https://www.linkedin.com/in/ellen-avrumova/" label="LinkedIn" style={{ margin:15, left:100 }}/></Col>
+            <Col><SocialIcon url="https://github.com/ellenAvrumova" label="GitHub" style={{ margin:15, left:100 }}/></Col>
+            <Col><SocialIcon url="mailto:e.avrumova@gmail.com?" label="Gmail" style={{ margin:15, left:100 }} bgColor="#de5246"/></Col>
+          </Row>
+        </Container>
+
+        <Container>
+          <Row>
+            <Col sm={{ offset: 2, span:2 }}>
+              <img width="100%" src={hi} alt="hello" />
+            </Col>
+            <Col sm={{ offset: 2 }} ><br /><br /><h2><u>Ellen Avrumova</u></h2></Col>
+          </Row>
+          <Row>
+            <Col sm = {{ span: 4 }}>
+              <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem' }} />
+            </Col>
+            <Col md = {8}>
+              <p>Hello! I'm a freshman at Hunter College.<br />I'm part of the Daedalus Honors Program and<br />have coded since 10th grade.</p>
+
+            </Col>
+          </Row>
+        </Container>
+
+        {/* <Container>
+          <Row>
+            <Col>
+              Heyyy
+            </Col>
+          </Row>
+        </Container> */}
+
       </header>
     </div>
   );
