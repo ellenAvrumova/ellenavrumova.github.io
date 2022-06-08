9320596a7ff771c3e03a5b56ac12066ab3295a19
ellenAvrumova <e.avrumova@gmail.com>
2022-06-08 14:08:14 -0400

    Added tabs

--- src/App.js
+++ src/App.js
@@ -4,30 +4,43 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import myImage from './myImage.jpg'
+import { Resume } from './Components/Resume';
+import { Scroll } from './Components/Scroll'
 
 function App() {
   return (
       <div className="Page">
-        <div className="Intro">
-          <Container>
-            <Row>
-              <Col sm = {{ span: 4 }}>
-                <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem', marginTop: 50, marginLeft: -10 }} />
-              </Col>
-              <Col>
+          <header>
+            <nav>
+              <a href="#about" class="active">About me</a>
+              <a href="#work">Work Experience</a>
+              <a href="#contact">Contacts</a>
+            </nav>
+          </header>
+          <section id="about">
+            <div class="container reveal">
+              <Container>
                 <br />
-                <h2>ELLEN AVRUMOVA</h2>
-                <div id ="hello">
-                  <p>Hello! I'm a freshman at Hunter College. I'm part of the Daedalus Honors Program and have been coding since 10th grade.</p>
-                  <p><u>Currently I am: </u><br />Exploring career paths in technology!</p>
-                  <p><u>My Current Goal: </u><br />Gain experience through internships!</p>
-                </div>
-              </Col>
-              {/* <Col sm={{ offset: 2, span:2 }}>
-                <img width="100%" src={hi} alt="hello" />
-              </Col> */}
-            </Row>
-          </Container>
+                <br />
+                <Row>
+                  <Col sm = {{ span: 4 }}>
+                    <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem', marginTop: 50, marginLeft: -10 }} />
+                  </Col>
+                  <Col>
+                    <br />
+                    <h2>ELLEN AVRUMOVA</h2>
+                    <div id ="hello">
+                      <p>Hello! I'm a freshman at Hunter College. I'm part of the Daedalus Honors Program and have been coding since 10th grade.</p>
+                      <p><u>Currently I am: </u><br />Exploring career paths in technology!</p>
+                      <p><u>My Current Goal: </u><br />Gain experience through internships!</p>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </section>
+          
+        <div className="Intro">
         </div>
         <br />
         <br />
