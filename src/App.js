0856e56a67dbedbf2af95f5f87ae08116a5a955a
ellenAvrumova <e.avrumova@gmail.com>
2022-07-02 14:17:36 -0400

    Scrolling

--- src/App.js
+++ src/App.js
@@ -6,110 +6,133 @@ import Col from 'react-bootstrap/Col';
 import myImage from './myImage.jpg'
 import amex from './American-Express-logo.png'
 import { Socials } from './Components/Socials.js'
-import { Scroll } from './Components/Scroll.js'
 
 function App() {
-  <header>
-    <script>
-      <Scroll />
-    </script>
-  </header>
+  const scrollListener = () => {
+    let section = document.querySelectorAll("section");
+    let menu = document.querySelectorAll("header nav a");
+
+    window.onscroll = () => {
+      section.forEach((i) => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - 150;
+        let height = i.offsetHeight;
+        let id = i.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+          menu.forEach((link) => {
+            link.classList.remove("active");
+            document
+              .querySelector("header nav a[href*=" + id + "]")
+              .classList.add("active");
+          });
+        }
+      })
+    };
+    clearInterval(scrollTimer);
+  };
+  const scrollTimer = setInterval(scrollListener, 1000);
+
   return (
-    <body>
-      <div className="Page">
-          <header>
-            <nav>
-              <a href="#about" class="active">About me</a>
-              <a href="#work">Work Experience</a>
-              <a href="#contact">Contacts</a>
-            </nav>
-          </header>
-          <section id="about">
-            <div class="container reveal">
-              <Container>
-                <br />
-                <br />
-                <Row>
-                  <Col sm = {{ span: 5 }}>
-                    <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem', marginTop: 80, marginLeft: -40 }} />
-                  </Col>
-                  <Col>
-                    <br />
-                    <h2>ELLEN AVRUMOVA</h2>
-                    <div id ="hello">
-                      <p>Hello! I'm a freshman at Hunter College. I'm part of the Daedalus Honors Program and have been coding since 10th grade.</p>
-                      <p><u>Currently I am: </u><br />Exploring career paths in technology!</p>
-                      <p><u>My Current Goal: </u><br />Gain experience through internships!</p>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-          </section>
-          
-          <section id="work">
-            <div class="container reveal">
+    <div className="Page">
+        <header>
+          <nav>
+            <a href="#about" className="active">About me</a>
+            <a href="#work">Work Experience</a>
+            <a href="#contact">Contacts</a>
+          </nav>
+        </header>
+
+        <section id="about">
+          <div className="container reveal">
+            <Container>
               <br />
               <br />
-              <Container>
-                <Row>
-                  <Col sm = {{ offset: 4 }}>
-                    <div id="header">
-                      <p>WORK EXPERIENCE</p>
-                    </div>
-                  </Col>
-                </Row>
-                <Row>
-                  {/* Insert Col here when adding new job */}
-                  <Col>
-                    <div id="internName">
-                      <p><u>Software Engingeer Intern</u></p>
-                    </div>
-                      <div id="list">
-                        <p>Used:</p>
-                        <ul>
-                          <li>Couchbase</li>
-                          <li>NoSQL</li>
-                          <li>JavaScript</li>
-                          <li>Postman</li>
-                          <li>React.js</li>
-                        </ul>
-                      </div>
-                  </Col>
-                  <Col sm = {{ span: 3 }}>
-                    <img width="100%" src={amex} className='amex' alt="amex" style={{ maxWidth: '24rem', marginLeft: -380, marginTop: 40 }} />
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-          </section>
-          
-          <section id="contact">
-            <div class="container reveal">
-              <br />
-              <br />
-              <Container>
-                <Row>
-                  <Col sm = {{ offset: 5 }}>
-                  <div id="header">
-                    <p>CONTACTS</p>
+              <Row>
+                <Col sm = {{ span: 5 }}>
+                  <img width="100%" src={myImage} className='img-thumbnail' alt="Me" style={{ maxWidth: '24rem', marginTop: 80, marginLeft: -40 }} />
+                </Col>
+                <Col>
+                  <br />
+                  <h2>ELLEN AVRUMOVA</h2>
+                  <div id ="hello">
+                    <p>Hello! I'm a freshman at Hunter College. I'm part of the Daedalus Honors Program and have been coding since 10th grade.</p>
+                    <p><u>Currently I am: </u><br />Exploring career paths in technology!</p>
+                    <p><u>My Current Goal: </u><br />Gain experience through internships!</p>
                   </div>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Socials />
-                  </Col>
-                </Row>
-                <Row>
-                  <p>Hey little buddy, you're a little buddy.</p>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </Row>
-              </Container>
-            </div>
-            </section>
-        </div>
-      </body>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </section>
+        
+        <section id="work">
+          <div className="container reveal">
+            <br />
+            <br />
+            <Container>
+              <Row>
+                <Col sm = {{ offset: 4 }}>
+                  <div id="header">
+                    <p>WORK EXPERIENCE</p>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                {/* Insert Col here when adding new job */}
+                <Col>
+                  <div id="internName">
+                    <p><u>Software Engingeer Intern</u></p>
+                  </div>
+                    <div id="list">
+                      <p>Used:</p>
+                      <ul>
+                        <li>Couchbase</li>
+                        <li>NoSQL</li>
+                        <li>JavaScript</li>
+                        <li>Postman</li>
+                        <li>React.js</li>
+                      </ul>
+                    </div>
+                </Col>
+                <Col sm = {{ span: 3 }}>
+                  <img width="100%" src={amex} className='amex' alt="amex" style={{ maxWidth: '24rem', marginLeft: -380, marginTop: 40 }} />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </section>
+        
+        <section id="contact">
+          <div className="container reveal">
+            <br />
+            <br />
+            <Container>
+              <Row>
+                <Col sm = {{ offset: 5 }}>
+                <div id="header">
+                  <p>CONTACTS</p>
+                </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Socials />
+                </Col>
+              </Row>
+              <Row>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                   has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                    took a galley of type and scrambled it to make a type specimen book. It has survived not
+                     only five centuries, but also the leap into electronic typesetting, remaining essentially
+                      unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+                       Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker including versions of Lorem Ipsum.</p>
+              </Row>
+            </Container>
+          </div>
+          </section>
+      </div>
   );
 }
 
